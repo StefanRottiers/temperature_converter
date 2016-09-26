c56ec15d7a7ac1d14913c5c7b7f85bba5cb8cc60
Stefan Rottiers <stefan.rottiers@hotmail.be>
2016-09-26 12:50:23 +0200

    WIP

--- app.rb
+++ app.rb
@@ -1,14 +1,18 @@
 require './lib/temperature_converter.rb'
-
+require "open-uri"
 
 input = ARGV.first.to_f
 
-commandline_temperatureARGV = TemperatureConverter.new input
+temperature_converter = TemperatureConverter.new input
 
+puts "Converted values from commandline"
+puts temperature_converter.convert_to_fahrenheit
+puts temperature_converter.convert_to_kelvin
+puts temperature_converter.convert_to_celcius + "\r\n\n"
+puts "Values from txt file"
+puts temperature_converter.from_file_temp_fahrenheit
+puts temperature_converter.from_file_temp_kelvin
+puts temperature_converter.from_file_temp_celcius
+puts "Values from url"
+commandline_temperatureARGV = TemperatureConverter.new open('http://labict.be/software-engineering/temperature/api/temperature/fake') {|a| a.read }
 puts commandline_temperatureARGV.convert_to_fahrenheit
-puts commandline_temperatureARGV.convert_to_kelvin
-puts commandline_temperatureARGV.convert_to_celcius
-puts commandline_temperatureARGV.from_file_temp
-# puts commandline_temperatureARGV.from_url_temp
-page_content = open('http://labict.be/software-engineering/temperature/api/temperature/fake').to_f
-puts page_content
--- lib/temperature_converter.rb
+++ lib/temperature_converter.rb
@@ -1,37 +1,37 @@
-require 'net/http'
-require 'uri'
-
-
 class TemperatureConverter
 
-  def initialize temp
-    @temp = temp
+  private
 
+  def initialize temp
+    @temp = temp.to_f
   end
 
+  public
+
   def convert_to_fahrenheit
-      ((@temp * 1.8)+32).to_s + " °F"
+      ((@temp * 1.8)+32).to_s + "°F"
   end
 
   def convert_to_kelvin
-    (@temp + 273.15).to_s + " °K"
+    (@temp + 273.15).to_s + "°K"
   end
 
   def convert_to_celcius
-    (@temp).to_s + " °C"
+    (@temp).to_s + "°C"
   end
 
-  def from_file_temp
+  def from_file_temp_fahrenheit
+    @temp=((File.read("./data/temp.txt").to_f*1.8)+32).to_s + " °F"
+  end
+
+  def from_file_temp_kelvin
+    @temp=(File.read("./data/temp.txt").to_f+273.15).to_s + " °K"
+  end
+
+  def from_file_temp_celcius
     @temp=File.read("./data/temp.txt").to_s + " °C"
   end
 
-  def open(url)
-    Net::HTTP.get(URI.parse(url))
-  end
-
-  # def from_url_temp
-  #   @temp = open(http://labict.be/software-engineering/temperature/api/temperature/fake).to_f
-  # end
 
 
 
