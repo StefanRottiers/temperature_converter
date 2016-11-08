3392bfe4c097223dda4f5a6f0dffbeccbbc7f050
Stefan Rottiers <stefan.rottiers@hotmail.be>
2016-11-08 17:26:16 +0100

    Waarden van server halen na chatserver te maken

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require './lib/temperature_converter.rb'
 require "open-uri"
+require 'rubygems'
+require 'mqtt'
+require 'json'
+
 
 input = ARGV.first.to_f
 
@@ -17,4 +21,17 @@ puts temperature_converter.from_file_temp_celcius + "\r\n\n"
 puts "Values from url"
 puts temperature_converter.from_url_temp_fahrenheit
 puts temperature_converter.from_url_temp_kelvin
-puts temperature_converter.from_url_temp_celcius
+puts temperature_converter.from_url_temp_celcius + "\r\n\n"
+
+puts "Values from server"
+client = MQTT::Client.connect(
+  :host => 'staging.thethingsnetwork.org',
+  :port => 1883,
+  :username =>'70B3D57ED00012B2',
+  :password =>'c8iuTSccnypK1eoFzEb/OoqB2FVAiFg/aEaYesnNf4w=',
+)
+
+client.get('70B3D57ED00012B2/devices/00000000AE6C63E4/up') do |topic,message|
+puts a  = JSON.parse(message)['fields']['temperature']
+#puts topic
+end
--- lib/temperature_converter.rb
+++ lib/temperature_converter.rb
@@ -45,7 +45,4 @@ class TemperatureConverter
   end
 
 
-
-
-
 end
