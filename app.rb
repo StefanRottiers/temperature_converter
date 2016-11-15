f5b9ceda9b523a36208be8d4eb36e18206df25d7
Stefan Rottiers <stefan.rottiers@hotmail.be>
2016-11-15 16:32:36 +0100

    bijna goed

--- app.rb
+++ app.rb
@@ -14,17 +14,27 @@ puts temperature_converter.convert_to_fahrenheit
 puts temperature_converter.convert_to_kelvin
 puts temperature_converter.convert_to_celcius + "\r\n\n"
 puts "{celsius: " +temperature_converter.convert_to_celcius + ", fahrenheit: " +temperature_converter.convert_to_fahrenheit + ", kelvin: " + temperature_converter.convert_to_kelvin + "}\r\n\n"
-puts "<div>Celcius" + temperature_converter.convert_to_celcius + " </div> "
-puts "<div>Celcius" + temperature_converter.convert_to_fahrenheit + " </div> "
-puts "<div>Celcius" + temperature_converter.convert_to_kelvin + " </div> "
+puts "<div>Celcius: " + temperature_converter.convert_to_celcius + " </div> "
+puts "<div>Fahrenheit: " + temperature_converter.convert_to_fahrenheit + " </div> "
+puts "<div>Kelvin: " + temperature_converter.convert_to_kelvin + " </div>\r\n\n "
+
 puts "Values from txt file"
 puts temperature_converter.from_file_temp_fahrenheit
 puts temperature_converter.from_file_temp_kelvin
 puts temperature_converter.from_file_temp_celcius + "\r\n\n"
+puts "{celsius: " +temperature_converter.from_file_temp_celcius + ", fahrenheit: " +temperature_converter.from_file_temp_fahrenheit + ", kelvin: " + temperature_converter.from_file_temp_kelvin + "}\r\n\n"
+puts "<div>Celcius: " + temperature_converter.from_file_temp_celcius + " </div> "
+puts "<div>Fahrenheit: " + temperature_converter.from_file_temp_fahrenheit + " </div> "
+puts "<div>Kelvin: " + temperature_converter.from_file_temp_kelvin + " </div>\r\n\n "
+
 puts "Values from url"
 puts temperature_converter.from_url_temp_fahrenheit
 puts temperature_converter.from_url_temp_kelvin
 puts temperature_converter.from_url_temp_celcius + "\r\n\n"
+puts "{celsius: " +temperature_converter.from_url_temp_celcius + ", fahrenheit: " +temperature_converter.from_url_temp_fahrenheit + ", kelvin: " + temperature_converter.from_url_temp_kelvin + "}\r\n\n"
+puts "<div>Celcius: " + temperature_converter.from_url_temp_celcius + " </div> "
+puts "<div>Fahrenheit: " + temperature_converter.from_url_temp_fahrenheit + " </div> "
+puts "<div>Kelvin: " + temperature_converter.from_url_temp_kelvin + " </div>\r\n\n "
 
 puts "Values from server"
 client = MQTT::Client.connect(
